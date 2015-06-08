89e618a3b98fa0dc79cfdba277ff34b70a675195
ephtracy <ephtracy@gmail.com>
2015-06-08 18:40:53 -0400

    Update cv_op.h

--- cv_op.h
+++ cv_op.h
@@ -9,7 +9,7 @@
 #define Moc_cv_op_h
 
 template< class T >
-cv::Point3_<T> operator/( const cv::Point3_<T> &a, float s ) {
+cv::Point3_<T> operator/( const cv::Point3_<T> &a, T s ) {
 	s = 1.0f / s;
 	return cv::Point3_<T>( a.x * s, a.y * s, a.z * s );
 }
@@ -17,14 +17,14 @@ cv::Point3_<T> operator/( const cv::Point3_<T> &a, float s ) {
 template< class T >
 cv::Point3_<T> operator*( const cv::Mat &m, const cv::Point3_<T> &v ) {
 	cv::Point3_<T> t;
-	t.x = m.at<T>( 0, 0 ) * v.x + m.at<T>( 0, 1 ) * v.y + m.at<T>( 0, 2 ) * v.z;
-	t.y = m.at<T>( 1, 0 ) * v.x + m.at<T>( 1, 1 ) * v.y + m.at<T>( 1, 2 ) * v.z;
-	t.z = m.at<T>( 2, 0 ) * v.x + m.at<T>( 2, 1 ) * v.y + m.at<T>( 2, 2 ) * v.z;
+	t.x = m.at<double>( 0, 0 ) * v.x + m.at<double>( 0, 1 ) * v.y + m.at<double>( 0, 2 ) * v.z;
+	t.y = m.at<double>( 1, 0 ) * v.x + m.at<double>( 1, 1 ) * v.y + m.at<double>( 1, 2 ) * v.z;
+	t.z = m.at<double>( 2, 0 ) * v.x + m.at<double>( 2, 1 ) * v.y + m.at<double>( 2, 2 ) * v.z;
 
 	if ( m.size().width == 4 ) {
-		t.x += m.at<T>( 0, 3 );
-		t.y += m.at<T>( 1, 3 );
-		t.z += m.at<T>( 2, 3 );
+		t.x += m.at<double>( 0, 3 );
+		t.y += m.at<double>( 1, 3 );
+		t.z += m.at<double>( 2, 3 );
 	}
 
 	return t;
@@ -41,13 +41,13 @@ cv::Point3_<T> Sum( const vector< cv::Point3_<T> > &v ) {
 
 template< class T >
 cv::Mat Outer( const cv::Point3_<T> &a, const cv::Point3_<T> &b ) {
-	cv::Mat t = cv::Mat_<T>( 3, 3 );
+	cv::Mat t = cv::Mat_<double>( 3, 3 );
 
 	const T *aa = &a.x;
 	const T *bb = &b.x;
 	for ( int i = 0; i < 3; i++ ) {
 		for ( int j = 0; j < 3; j++ ) {
-			t.at<T>( i, j ) = aa[ i ] * bb[ j ];
+			t.at<double>( i, j ) = aa[ i ] * bb[ j ];
 		}
 	}
 
@@ -62,22 +62,22 @@ T Length( const cv::Point3_<T> &v ) {
 // rotate firstly, translate next
 template< class T >
 cv::Mat TranslateRotate( const cv::Point3_<T> &t, const cv::Mat &r ) {
-	cv::Mat m = cv::Mat_<T>( 4, 4 );
+	cv::Mat m = cv::Mat_<double>( 4, 4 );
 
 	for ( int i = 0; i < 3; i++ ) {
 		for ( int j = 0; j < 3; j++ ) {
-			m.at<T>( i, j ) = r.at<T>( i, j );
+			m.at<double>( i, j ) = r.at<double>( i, j );
 		}
 	}
 
-	m.at<T>( 0, 3 ) = t.x;
-	m.at<T>( 1, 3 ) = t.y;
-	m.at<T>( 2, 3 ) = t.z;
+	m.at<double>( 0, 3 ) = t.x;
+	m.at<double>( 1, 3 ) = t.y;
+	m.at<double>( 2, 3 ) = t.z;
 
-	m.at<T>( 3, 0 ) = 0;
-	m.at<T>( 3, 1 ) = 0;
-	m.at<T>( 3, 2 ) = 0;
-	m.at<T>( 3, 3 ) = 1;
+	m.at<double>( 3, 0 ) = 0;
+	m.at<double>( 3, 1 ) = 0;
+	m.at<double>( 3, 2 ) = 0;
+	m.at<double>( 3, 3 ) = 1;
 
 	return m;
 }
@@ -100,26 +100,21 @@ cv::Mat QuatToMat( T x, T y, T z, T w ) {
 	T wy2 = w * y2;
 	T wz2 = w * z2;
 
-	cv::Mat m = cv::Mat_< T >( 3, 3 );
+	cv::Mat m = cv::Mat_<double>( 3, 3 );
 
-	m.at<T>( 0, 0 ) = 1 - yy2 - zz2;
-	m.at<T>( 0, 1 ) =     xy2 - wz2;
-	m.at<T>( 0, 2 ) =     zx2 + wy2;
+	m.at<double>( 0, 0 ) = 1 - yy2 - zz2;
+	m.at<double>( 0, 1 ) =     xy2 - wz2;
+	m.at<double>( 0, 2 ) =     zx2 + wy2;
 
-	m.at<T>( 1, 0 ) =     xy2 + wz2;
-	m.at<T>( 1, 1 ) = 1 - zz2 - xx2;
-	m.at<T>( 1, 2 ) =     yz2 - wx2;
+	m.at<double>( 1, 0 ) =     xy2 + wz2;
+	m.at<double>( 1, 1 ) = 1 - zz2 - xx2;
+	m.at<double>( 1, 2 ) =     yz2 - wx2;
 
-	m.at<T>( 2, 0 ) =     zx2 - wy2;
-	m.at<T>( 2, 1 ) =     yz2 + wx2;
-	m.at<T>( 2, 2 ) = 1 - xx2 - yy2;
+	m.at<double>( 2, 0 ) =     zx2 - wy2;
+	m.at<double>( 2, 1 ) =     yz2 + wx2;
+	m.at<double>( 2, 2 ) = 1 - xx2 - yy2;
 
 	return m;
-}
-
-template< class T >
-T Clamp( T v, T bmin, T bmax ) {
-	return ( v <= bmin ? bmin : ( v >= bmax ? bmax : v ) );
 }
 
 #endif // Moc_cv_op_h
