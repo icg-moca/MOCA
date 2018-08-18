e591c83772aa025b0df87106cadb0e83428549e2
ephtracy <ephtracy@gmail.com>
2018-08-17 22:13:27 -0400

    rigid

---
+++ moca/proj-marker-transform/proj-marker-transform/proj-marker-transform.cpp
@@ -0,0 +1,222 @@
+// proj-marker-transform.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+#include "stdafx.h"
+
+#include <vector>
+#include <iostream>
+#include <time.h>
+
+#include <Eigen/Dense>
+#include <Eigen/SVD>
+#include <Eigen/Cholesky>
+
+#include <Eigen/Sparse>
+#include <Eigen/SparseCholesky>
+
+#include <math/func.h>
+#include <math/matrix.h>
+#include <math/SparseBlockSquareMatrix.h>
+
+float icp_point_to_point_svd(
+	const std::vector< Eigen::Vector3f > &src_points,
+	const std::vector< Eigen::Vector3f > &dst_points,
+	Eigen::Matrix3f &out_r, // rotation
+	Eigen::Vector3f &out_t  // translation
+) {
+	if ( src_points.empty() ) {
+		out_r.setIdentity();
+		out_t.setZero();
+		return 0.0f;
+	}
+		
+	// E(src)
+	Eigen::Vector3f src_mean = mean( src_points );
+
+	// E(dst)
+	Eigen::Vector3f dst_mean = mean( dst_points );
+
+	// A = Sum( ( src - E(src) ) * ( dst - E(dst) )' )
+	Eigen::Matrix3f A = Eigen::Matrix3f::Identity() * 1e-6f;
+	for ( int i = 0; i < src_points.size(); i++ ) {
+		A += ( src_points[i] - src_mean ) * ( dst_points[i] - dst_mean ).transpose();
+	}
+
+	// SVD : A = U * D * V'
+	Eigen::JacobiSVD<Eigen::MatrixXf> svd(A, Eigen::ComputeThinU | Eigen::ComputeThinV);
+
+	// R = V * U'
+	out_r = svd.matrixV() * svd.matrixU().transpose();
+
+	// T = E(dst) - R * E(src)
+	out_t = dst_mean - out_r * src_mean;
+
+	// rme
+	return rme( src_points, dst_points, out_r, out_t );
+}
+
+void init_affine_transforms( int size, Eigen::VectorXf &x, const Eigen::Matrix3f &rot, const std::vector< Eigen::Vector3f > &src, const std::vector< Eigen::Vector3f > &dst ) {
+	for ( int i = 0; i < size; i++ ) {
+		x.block( 12 * i + 3 * 0, 0, 3, 1 ) = rot.col(0); Eigen::Vector3f( 1, 0, 0 );
+		x.block( 12 * i + 3 * 1, 0, 3, 1 ) = rot.col(1); Eigen::Vector3f( 0, 1, 0 );
+		x.block( 12 * i + 3 * 2, 0, 3, 1 ) = rot.col(2); Eigen::Vector3f( 0, 0, 1 );
+		x.block( 12 * i + 3 * 3, 0, 3, 1 ) = dst[i] - src[i];
+	}
+}
+
+void EvalLinearSystem( int size, Eigen::MatrixXf &JtJ, Eigen::VectorXf &Jtr, Eigen::VectorXf &x, std::vector<Eigen::Vector3f> &src, std::vector<Eigen::Vector3f> &dst, float &E ) {	
+	// E-rigid
+	float w_rigid = 1.0f;
+	{
+		for ( int i = 0; i < size; i++ ) {
+			Eigen::Vector3f M[4];
+			M[0] = x.block( i * 12 + 0 * 3, 0, 3, 1 );
+			M[1] = x.block( i * 12 + 1 * 3, 0, 3, 1 );
+			M[2] = x.block( i * 12 + 2 * 3, 0, 3, 1 );
+			M[3] = x.block( i * 12 + 3 * 3, 0, 3, 1 );
+
+			// ck dot ck - 1
+			for ( int k = 0; k < 3; k++ ) {
+				float r = M[k].dot( M[k] ) - 1;
+			
+				Eigen::VectorXf J = Eigen::VectorXf::Zero( 12 );
+				J.block( k * 3, 0, 3, 1 ) = M[k] + M[k];
+
+				E += r * r * w_rigid;
+				JtJ.block( i * 12, i * 12, 12, 12 ) += J * J.transpose() * w_rigid; // block (i, i)
+				Jtr.block( i * 12, 0, 12, 1 ) += J * r * w_rigid; // block ( i )
+			}
+
+			// ck dot cj
+			for ( int k = 0; k < 3; k++ ) {
+				int j = ( k + 1 ) % 3;
+
+				float r = M[k].dot( M[j] );
+
+				E += r * r * w_rigid;
+
+				Eigen::VectorXf J = Eigen::VectorXf::Zero( 12 );
+
+				J.block( k * 3, 0, 3, 1 ) = M[j];
+				J.block( j * 3, 0, 3, 1 ) = M[k];
+
+				JtJ.block( i * 12, i * 12, 12, 12 ) += J * J.transpose() * w_rigid; // block (i, i)
+				Jtr.block( i * 12, 0, 12, 1 ) += J * r * w_rigid; // block ( i )
+			}
+		}
+	}
+	printf( "E-rigid : %f\n", E );
+
+	// E-smooth
+	float w_smooth = 1.0f;
+	{
+		for ( int i = 0; i < size; i++ ) {
+		for ( int j = 0; j < size; j++ ) {
+			if ( i == j ) {
+				continue;
+			}
+			Eigen::Vector3f Mi[4], Mj[4];
+			Mi[0] = x.block( i * 12 + 0 * 3, 0, 3, 1 );
+			Mi[1] = x.block( i * 12 + 1 * 3, 0, 3, 1 );
+			Mi[2] = x.block( i * 12 + 2 * 3, 0, 3, 1 );
+			Mi[3] = x.block( i * 12 + 3 * 3, 0, 3, 1 );
+
+			Mj[0] = x.block( j * 12 + 0 * 3, 0, 3, 1 );
+			Mj[1] = x.block( j * 12 + 1 * 3, 0, 3, 1 );
+			Mj[2] = x.block( j * 12 + 2 * 3, 0, 3, 1 );
+			Mj[3] = x.block( j * 12 + 3 * 3, 0, 3, 1 );
+
+			// Ri * ( gj - gi ) - ( gj - gi ) - ( tj - ti )
+
+			Eigen::Vector3f dg = src[j] - src[i];
+			Eigen::Vector3f dt = Mj[3] - Mi[3];
+
+			Eigen::Vector3f r = ( dg[0] * Mi[0] + dg[1] * Mi[1] + dg[2] * Mi[2] ) - dg - dt;
+
+			E += r.dot( r ) * w_smooth;
+
+			Eigen::MatrixXf Ji = Eigen::MatrixXf::Zero( 3, 12 );
+			Ji( 0, 0 + 3 * 0 ) = dg[0]; Ji( 0, 0 + 3 * 1 ) = dg[1]; Ji( 0, 0 + 3 * 2 ) = dg[2];
+			Ji( 1, 1 + 3 * 0 ) = dg[0];	Ji( 1, 1 + 3 * 1 ) = dg[1];	Ji( 1, 1 + 3 * 2 ) = dg[2];
+			Ji( 2, 2 + 3 * 0 ) = dg[0];	Ji( 2, 2 + 3 * 1 ) = dg[1];	Ji( 2, 2 + 3 * 2 ) = dg[2];
+
+		//	std::cout << Ji <<std::endl;
+
+			JtJ.block( i * 12, i * 12, 12, 12 ) += Ji.transpose() * Ji * w_smooth;
+			Jtr.block( i * 12, 0, 12, 1 ) += Ji.transpose() * r * w_smooth;
+		}}
+	}
+	printf( "E-smooth : %f\n", E );
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	int size = 3;
+	
+	std::vector<Eigen::Vector3f> src( size ), dst( size );
+
+	// gen transform
+	Eigen::Vector3f euler;
+	euler << 20, 50, -45;
+	Eigen::Matrix3f rot = euler_to_mat( radians( euler ) );
+	Eigen::Vector3f t;
+	t << 30, 50, -10;
+	std::cout << "Truth" << std::endl;
+	std::cout << rot << std::endl;
+	std::cout << t << std::endl;
+
+	// gen data
+	for ( int i = 0; i < size; i++ ) {
+		Eigen::Vector3f euler;
+		euler << 20, 50, -45;
+		euler[1] += i * 5.0f;
+		Eigen::Matrix3f rot = euler_to_mat( radians( euler ) );
+
+		Eigen::Vector3f t;
+		t << 30, 50, -10;
+
+		src[i] = Eigen::Vector3f::Random();
+		src[i][0] = fmod( src[i][0], 256.0f );
+		src[i][1] = fmod( src[i][1], 256.0f );
+		src[i][2] = fmod( src[i][2], 256.0f );
+		dst[i] = rot * src[i] + t;
+		//std::cout << "i << std::endl;
+		//std::cout << src[i] << std::endl;
+		//std::cout << dst[i] << std::endl;
+	}
+		
+	// rigid transform
+	Eigen::Matrix3f out_rot = Eigen::Matrix3f::Identity();
+	Eigen::Vector3f out_t = Eigen::Vector3f::Zero();;
+	{
+		float rme = icp_point_to_point_svd( src, dst, out_rot, out_t );
+
+		std::cout << "Result" << std::endl;
+		std::cout << out_rot << std::endl;
+		std::cout << out_t << std::endl;
+		std::cout << "rme : " << rme << std::endl;
+		std::cout << std::endl;
+	}
+
+	Eigen::MatrixXf JtJ = Eigen::MatrixXf::Identity( 12 * size, 12 * size ) * 1e-6f;
+	Eigen::VectorXf Jtr = Eigen::VectorXf::Zero( 12 * size );
+	Eigen::VectorXf x( 12 * size );
+	float E = 0.0f;
+
+	init_affine_transforms( size, x, out_rot, src, dst );
+
+	for ( int i = 0; i < 5; i++ ) {
+		JtJ = Eigen::MatrixXf::Identity( 12 * size, 12 * size ) * 1e-6f;
+		Jtr = Eigen::VectorXf::Zero( 12 * size );
+		E = 0.0f;
+	
+		EvalLinearSystem( size, JtJ, Jtr, x, src, dst, E );
+		//std::cout << Jtr;
+		x -= JtJ.fullPivHouseholderQr().solve( Jtr );
+	}
+
+	return 0;
+}
+
--- moca/src/math/matrix.h
+++ moca/src/math/matrix.h
@@ -1,5 +1,12 @@
-#ifndef misc_h
-#define misc_h
+#ifndef matrix_h
+#define matrix_h
+
+const float PI = 3.1415926535897932384626433832795;
+
+template< class T >
+T radians( const T &degree ) {
+	return degree * ( PI / 180.0f );
+}
 
 static void mad_mat_mult_v( int size, const float *B, const float *v, float *r ) {
 	for ( int row = 0; row < size; row++ ) {
@@ -32,5 +39,119 @@ void random_positive_definite_matrix( int size, Eigen::MatrixXf &A ) {
 float rme( const Eigen::VectorXf &a, const Eigen::VectorXf &b ) {
 	return sqrtf( ( a - b ).dot( a - b ) / a.rows() );
 }
+
+// R(x)
+Eigen::Matrix3f rot_x_mat( float rad ) {
+	float c = cos( rad );
+	float s = sin( rad );
+
+	Eigen::Matrix3f m;
+	m <<
+		1, 0, 0,
+		0, c, -s,
+		0, s, c
+		;
+
+	return m;
+}
+
+// R(y)
+Eigen::Matrix3f rot_y_mat( float rad ) {
+	float c = cos( rad );
+	float s = sin( rad );
+
+	Eigen::Matrix3f m;
+	m <<
+		c, 0, s,
+		0, 1, 0,
+		-s, 0, c
+		;
+
+	return m;
+}
+
+// R(z)
+Eigen::Matrix3f rot_z_mat( float rad ) {
+	float c = cos( rad );
+	float s = sin( rad );
+
+	Eigen::Matrix3f m;
+	m <<
+		c, -s, 0,
+		s, c, 0,
+		0, 0, 1
+		;
+
+	return m;
+}
+
+// R(z) * R(y) * R(x)
+Eigen::Matrix3f euler_to_mat( const Eigen::Vector3f &euler ) {
+	return rot_z_mat( euler.z() ) * rot_y_mat( euler.y() ) * rot_x_mat( euler.x() );
+}
+
+Eigen::Matrix3f euler_to_skew( const Eigen::Vector3f &euler ) {
+	float x = euler.x();
+	float y = euler.y();
+	float z = euler.z();
+
+	Eigen::Matrix3f r;
+	r <<
+		1.0f, -z, +y,
+		+z, 1.0f, -x,
+		-y, +x, 1.0f;
+
+	return r;
+}
+
+// skew( w ) * v = cross( w, v )
+// Infinitesimal rotations = skew( w ) + I
+Eigen::Matrix3f skew_mat( const Eigen::Vector3f &v ) {
+	float x = v.x();
+	float y = v.y();
+	float z = v.z();
+	
+	Eigen::Matrix3f sk;
+	sk <<
+		 0, -z, +y,
+		+z,  0, -x,
+		-y, +x,  0
+	;
+
+	return sk;
+}
+
+// E(v)
+Eigen::Vector3f mean( const std::vector< Eigen::Vector3f > &points ) {
+	if ( points.empty() ) {
+		return Eigen::Vector3f::Zero();
+	}
+	
+	Eigen::Vector3f e;
+	e.setZero();
+	for ( int i = 0; i < points.size(); i++ ) {
+		e += points[i];
+	}
+	e /= (float)( points.size() );
+
+	return e;
+}
+
+// sqrt( E( | dst - r * src - t |^2 ) )
+float rme( const std::vector< Eigen::Vector3f > &src, const std::vector< Eigen::Vector3f > &dst, const Eigen::Matrix3f &r, const Eigen::Vector3f &t ) {
+	if ( src.empty() ) {
+		return 0.0f;
+	}
+	
+	float e = 0.0f;
+	for ( int i = 0; i < src.size(); i++ ) {
+		Eigen::Vector3f d = dst[i] - r * src[i] - t;
+		e += d.dot( d );
+	}
+	e /= (float)( src.size() );
+
+	return sqrt( e );
+}
+
 
 #endif
